ce22abecae909b536fd4c6c2f930991f2a9ecfc2
rdwrds <r.edwards1492@gmail.com>
2025-04-07 15:44:06 -0500

    init assets/data

---
+++ src/data/locations.js
@@ -0,0 +1,81 @@
+export const ATAKHAN = [
+  //top atakhan
+  { x: 137, y: 140 },
+  //bot atakhan
+  { x: 387, y: 403 },
+];
+
+export const BARON_PIT = [
+  //baron/herald/grubs
+  { x: 208, y: 186 },
+];
+
+export const DRAGON = [
+  //dargon
+  { x: 455, y: 440 },
+];
+
+export const BUFFS = [
+  { x: 163, y: 274 }, //blue
+  { x: 307, y: 422 }, //red
+  { x: 286, y: 163 }, //red
+  { x: 430, y: 315 }, //blue
+];
+
+export const CAMPS = [
+  //topside
+  { x: 105, y: 258 },
+
+  { x: 164, y: 330 },
+  { x: 282, y: 370 },
+
+  { x: 332, y: 469 },
+  { x: 263, y: 118 },
+
+  { x: 310, y: 214 },
+  { x: 428, y: 261 },
+
+  { x: 488, y: 322 },
+];
+
+export const STRUCTURES = {
+  RED_SIDE: [
+    //topside
+    { x: 185, y: 60 },
+    { x: 315, y: 80 },
+    { x: 405, y: 70 },
+    { x: 435, y: 70 },
+    { x: 485, y: 90 },
+    { x: 505, y: 80 },
+    { x: 500, y: 110 },
+    { x: 520, y: 154 },
+    { x: 520, y: 181 },
+    { x: 510, y: 269 },
+    { x: 530, y: 400 },
+    //topside mid
+    { x: 352, y: 254 },
+    { x: 380, y: 200 },
+    { x: 430, y: 160 },
+    { x: 445, y: 140 },
+  ],
+
+  BLUE_SIDE: [
+    //botside
+    { x: 407, y: 522 },
+    { x: 277, y: 512 },
+    { x: 180, y: 520 },
+    { x: 150, y: 518 },
+    { x: 105, y: 500 },
+    { x: 83, y: 502 },
+    { x: 90, y: 483 },
+    { x: 68, y: 434 },
+    { x: 68, y: 411 },
+    { x: 80, y: 321 },
+    { x: 62, y: 184 },
+    //botside mid
+    { x: 148, y: 448 },
+    { x: 158, y: 430 },
+    { x: 210, y: 390 },
+    { x: 240, y: 330 },
+  ],
+};
