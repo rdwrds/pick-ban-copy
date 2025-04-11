fdf2302dbd22afcc01d65fb9d1c6f8e77da407b3
rdwrds <r.edwards1492@gmail.com>
2025-04-10 19:26:13 -0500

    renames, added structs

--- src/data/locations.js
+++ src/data/locations.js
@@ -2,7 +2,7 @@ export const ATAKHAN = [
   //top atakhan
   { x: 137, y: 140 },
   //bot atakhan
-  { x: 387, y: 403 },
+  { x: 455, y: 440 },
 ];
 
 export const BARON_PIT = [
@@ -10,9 +10,9 @@ export const BARON_PIT = [
   { x: 208, y: 186 },
 ];
 
-export const DRAGON = [
+export const DRAGON_PIT = [
   //dargon
-  { x: 455, y: 440 },
+  { x: 387, y: 403 },
 ];
 
 export const BUFFS = [
@@ -20,6 +20,12 @@ export const BUFFS = [
   { x: 307, y: 422 }, //red
   { x: 286, y: 163 }, //red
   { x: 430, y: 315 }, //blue
+];
+
+//hidden structures that act as clips for the base visions
+export const BASE_VISION = [
+  { x: 1, y: 532 },
+  { x: 532, y: 1 },
 ];
 
 export const CAMPS = [
@@ -44,11 +50,8 @@ export const STRUCTURES = {
     { x: 185, y: 60 },
     { x: 315, y: 80 },
     { x: 405, y: 70 },
-    { x: 435, y: 70 },
     { x: 485, y: 90 },
-    { x: 505, y: 80 },
     { x: 500, y: 110 },
-    { x: 520, y: 154 },
     { x: 520, y: 181 },
     { x: 510, y: 269 },
     { x: 530, y: 400 },
@@ -56,7 +59,12 @@ export const STRUCTURES = {
     { x: 352, y: 254 },
     { x: 380, y: 200 },
     { x: 430, y: 160 },
+  ],
+  RED_SIDE_NEXUS: [{ x: 505, y: 80 }],
+  RED_SIDE_INHIBITORS: [
     { x: 445, y: 140 },
+    { x: 435, y: 70 },
+    { x: 520, y: 154 },
   ],
 
   BLUE_SIDE: [
@@ -64,18 +72,21 @@ export const STRUCTURES = {
     { x: 407, y: 522 },
     { x: 277, y: 512 },
     { x: 180, y: 520 },
-    { x: 150, y: 518 },
+
     { x: 105, y: 500 },
-    { x: 83, y: 502 },
     { x: 90, y: 483 },
-    { x: 68, y: 434 },
     { x: 68, y: 411 },
     { x: 80, y: 321 },
     { x: 62, y: 184 },
     //botside mid
-    { x: 148, y: 448 },
     { x: 158, y: 430 },
     { x: 210, y: 390 },
     { x: 240, y: 330 },
+  ],
+  BLUE_SIDE_NEXUS: [{ x: 83, y: 502 }],
+  BLUE_SIDE_INHIBITORS: [
+    { x: 150, y: 518 },
+    { x: 68, y: 434 },
+    { x: 148, y: 448 },
   ],
 };
