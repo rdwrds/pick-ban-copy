d04f894da16356f6fba880817d45fffdd85d9450
rdwrds <r.edwards1492@gmail.com>
2025-04-30 15:36:25 -0500

    init/data

--- src/data/minimap_champ_icons.js
+++ src/data/minimap_champ_icons.js
@@ -2,6 +2,7 @@ const WEIRD_CHAMP_NAMES = {
   Ambessa: { name: "Ambessa", path: "ambessa_circle_0.domina.png" },
   Aurora: { name: "Aurora", path: "aurora_circle_0.aurora.png" },
   Mel: { name: "Mel", path: "mel_circle_0.mel.png" },
+  //Cassiopeia: { name: "Cassiopeia", path: "cassiopeia_circle_0.png" },
   Rammus: { name: "Rammus", path: "armordillo_circle.png" },
   Orianna: { name: "Orianna", path: "oriana_circle.png" },
   Teemo: { name: "Teemo", path: "teemo_circle_0.asu_teemo.png" },
---
+++ src/data/sights.js
@@ -0,0 +1,7 @@
+export const VISION_RANGES = {
+  CHAMPION: 1350,
+  MINION: 1200,
+  WARD: 900,
+  BLUE_WARD: 500,
+  GHOST_PORO: 450,
+};
